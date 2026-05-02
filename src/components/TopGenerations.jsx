11a2b862c1df0859f40d0e033d7a3db69c1b8e37
cameron Rimon <rimon072159@gmail.com>
2026-05-02 11:35:28 -0700

    Hasan

--- src/components/TopGenerations.jsx
+++ src/components/TopGenerations.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PhotoCard from './PhotoCard';
 
 const TopGenerations = async() => {
-    const res = await fetch('http://localhost:3000/data.json')
+    const res = await fetch('https://assignment-8-next-projects.vercel.app/data.json')
     const photos = await res.json()
     const topPhotos = photos.slice(0, 8)
     console.log(topPhotos);
