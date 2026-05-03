13a8769dafc7322ed74c62cf3c7d6e4a92734965
cameron Rimon <rimon072159@gmail.com>
2026-05-03 12:59:42 -0700

    added category

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,73 +1,40 @@
-// import Category from '@/components/Category';
-// import PhotoCard from '@/components/PhotoCard';
-// import React from 'react';
-
-// const CoursesPage = async ({searchParams}) => {
-//     const {Category} = await searchParams;
-   
-// const filteredPhotos = photos.filter(
-//   (photo) =>
-//     photo?.category?.toLowerCase() === category?.toLowerCase()
-// );
-
-
-//       const res = await fetch('https://assignment-8-next-projects.vercel.app/data.json')
-//     const photos = await res.json()
-
-
-    
-    
-//     return (
-//         <div>
-//             <h1 className='text-2xl font-bold m-4'>Courses</h1>
-
-//             <Category/>
-
-//             <div className='grid grid-cols-4 gap-5'>
-//                 {fillteredPhotos.map(photo=> <PhotoCard key={photo.id} photo={photo}></PhotoCard>)}
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default CoursesPage;
-
-
 import Category from '@/components/Category';
 import PhotoCard from '@/components/PhotoCard';
 import React from 'react';
 
-const CoursesPage = async ({ searchParams }) => {
-  const category = searchParams?.category || "";
+const CoursesPage = async ({searchParams}) => {
+    const {category} = await searchParams;
+    
+    console.log(category);
 
-  // fetch আগে
-  const res = await fetch(
-    'https://assignment-8-next-projects.vercel.app/data.json',
-    { cache: "no-store" }
-  );
-  const photos = await res.json();
+      const res = await fetch('https://assignment-8-next-projects.vercel.app/data.json')
+    const photos = await res.json()
+    
+  
+   const filteredPhotos = category ? photos.filter(photo => photo.category.toLowerCase()) : photos
 
-  // তারপর filter
-  const filteredPhotos = category
-    ? photos.filter(
-        (photo) =>
-          photo?.category?.toLowerCase() === category.toLowerCase()
-      )
-    : photos;
 
-  return (
-    <div>
-      <h1 className='text-2xl font-bold m-4'>Courses</h1>
 
-      <Category />
 
-      <div className='grid grid-cols-4 gap-5'>
-        {filteredPhotos.map((photo) => (
-          <PhotoCard key={photo.id} photo={photo} />
-        ))}
-      </div>
-    </div>
-  );
+
+
+    
+    
+    return (
+        <div>
+            <h1 className='text-2xl font-bold m-4'>Courses</h1>
+
+            <Category></Category>
+
+            <div className='grid grid-cols-4 gap-5'>
+                {filteredPhotos.map(photo=> <PhotoCard key={photo.id} photo={photo}></PhotoCard>)}
+            </div>
+        </div>
+    );
 };
 
 export default CoursesPage;
+
+
+
+
